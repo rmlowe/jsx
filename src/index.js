0227525e9ecaeeabe809bc1b90c05e2d54f49912
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-26 16:46:06 +0800

    What is JSX?

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 // Create a react component
 const App = () => {
   return <div>Hi there!</div>;
-}
+};
 
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
